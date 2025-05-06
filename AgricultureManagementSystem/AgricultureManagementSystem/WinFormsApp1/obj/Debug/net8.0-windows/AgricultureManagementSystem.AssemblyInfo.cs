1fa742152b2f145490e5b09af178ffdcd1c07847
Subhanmalik138 <subhan138malik@gmail.com>
2025-05-05 21:40:15 -0700

    improved verisoin

--- AgricultureManagementSystem/AgricultureManagementSystem/WinFormsApp1/obj/Debug/net8.0-windows/AgricultureManagementSystem.AssemblyInfo.cs
+++ AgricultureManagementSystem/AgricultureManagementSystem/WinFormsApp1/obj/Debug/net8.0-windows/AgricultureManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgricultureManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b36aeb697e714039cc037ad38c821ba76fefe80a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgricultureManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgricultureManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
